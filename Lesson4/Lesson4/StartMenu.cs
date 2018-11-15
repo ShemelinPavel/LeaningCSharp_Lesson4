fd41b690b7baf16e8ca0931366593c534848d9dc
Pavel Shemelin <pauls0@mail.ru>
2018-11-15 23:23:23 +0300

    Task1

---
+++ Lesson4/Lesson4/StartMenu.cs
@@ -0,0 +1,59 @@
+﻿namespace Lesson4
+{
+    using System;
+    using ClassLibraryLesson4;
+
+    partial class Lesson4
+    {
+
+        static void StartMenu()
+        {
+
+            string Welcome = "Добрый день, пользователь!\n";
+            Welcome = Welcome + "Выберите чем бы Вы хотели заняться:\n";
+            Welcome = Welcome + "Меню на сегодня:\n";
+            Welcome = Welcome + "1 - Работа массивом\n";
+            Welcome = Welcome + "2 - Подсчет числа знаков в числе\n";
+            Welcome = Welcome + "3 - Работа с дробями\n";
+
+            Welcome = Welcome + "0 - Выход из программы\n";
+
+            while (true)
+            {
+
+                ClassLibraryLesson4.PrintTaskWelcomeScreen(Welcome);
+
+                ConsoleKeyInfo userChooseKey = Console.ReadKey(true);
+
+                switch (userChooseKey.Key)
+                {
+
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        Task1();
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        ClassLibraryLesson4.Print("Делал в ДЗ №2");
+                        ClassLibraryLesson4.Pause("Нажмите любую клавишу.");
+                        break;
+
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        //Task3();
+                        break;
+
+                    case ConsoleKey.D0:
+                    case ConsoleKey.NumPad0:
+                        Environment.Exit(0); // на выход
+                        break;
+
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ Lesson4/Lesson4/Task1.cs
@@ -0,0 +1,72 @@
+﻿namespace Lesson4
+{
+
+    using System;
+    using ClassLibraryLesson4;
+
+    partial class Lesson4
+    {
+
+        public static void Task1()
+        {
+
+            string welcome = "Вы выбрали демонстрацию работы с массивом.\n";
+            welcome = welcome + "Это интересно!\n";
+            welcome = welcome + "Давайте начнем\n";
+
+            ClassLibraryLesson4.PrintTaskWelcomeScreen(welcome);
+
+            int[] arrayNumbers = new int[20];
+
+            //рандом генератор
+            Random rand = new Random();
+
+            //заполнение массива
+            for (int counter = 0; counter < arrayNumbers.Length; counter++)
+            {
+
+                arrayNumbers[counter] = rand.Next(-10000, 10000);
+
+            }
+
+            //вывод массива на экран
+            ClassLibraryLesson4.Print("Содержимое массива:");
+            foreach (var item in arrayNumbers)
+            {
+
+                ClassLibraryLesson4.Print(item.ToString() + " ", false);
+
+            }
+
+            ClassLibraryLesson4.Print("\n");
+
+            //перебор пар
+
+            ClassLibraryLesson4.Print("Анализируем пары...");
+
+            ushort counterPairs = 0;
+            const int denominator = 3;
+
+            for (int counter = 0; counter < arrayNumbers.Length - 1; counter++)
+            {
+
+                ClassLibraryLesson4.Print($"{arrayNumbers[counter]} {arrayNumbers[counter + 1]}");
+
+                if (arrayNumbers[counter] % denominator == 0 ^ arrayNumbers[counter + 1] % denominator == 0)
+                {
+
+                    counterPairs++;
+
+                }
+
+            }
+
+            ClassLibraryLesson4.Print("");
+            ClassLibraryLesson4.Print($"Найдено {counterPairs} пар, где только одно из значений делится на  {denominator}");
+            ClassLibraryLesson4.Pause("Нажмите любую клавишу.");
+
+        }
+
+    }
+
+}
